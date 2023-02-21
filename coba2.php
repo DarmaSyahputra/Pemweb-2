01b8543632229b0f0e8224371b5341e1c32e72fe
DarmaSyahputragit config --global user.name DarmaSyahputragit config --global user.name DarmaSyahputra <darmaasptr11@gmail.com>
2023-02-21 15:01:50 +0700

    tugas p1

---
+++ coba2.php
@@ -0,0 +1,6 @@
+<?php
+    $ar_buah = ["Pepaya", "Mangga","Pisang","Jambu"];
+    foreach($ar_buah as $buah){
+        echo $buah;
+    }
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    $nama = 'Darma Syahputra';
+    $umur = 22;
+    $berat = 67;
+
+    echo 'Nama saya : ' . $nama;
+    echo '<br/>Umur saya : ' . $umur;
+    echo '<br/>Berat saya : ' . $berat;
+?>
